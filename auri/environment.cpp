e3690631d548d09db48ac5ba7610cd62740d496e
uventura <ualivent@gmail.com>
2025-09-12 11:02:26 -0300

    Adding environment class

---
+++ auri/environment.cpp
@@ -0,0 +1,18 @@
+#include "auri/environment.hpp"
+
+namespace Auri {
+template <typename T>
+Environment<T>::Environment(std::string identifier,T value, Environment* previousEnv) {
+
+}
+
+template <typename T>
+T Environment<T>::get(std::string identifier) {
+
+}
+
+template <typename T>
+void Environment<T>::set(std::string identifier, T value) {
+    
+}
+}
---
+++ auri/environment.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <string>
+#include <map>
+
+namespace Auri {
+template <typename T>
+class Environment {
+    private:
+        std::string identifier_;
+        T value_;
+        Environment* previousEnv_;
+
+    public:
+        Environment(
+            std::string identifier,
+            T value,
+            Environment* previousEnv
+        );
+
+        T get(std::string identifier);
+        void set(std::string identifier, T value);
+};
+}
