68f6aa6234755b40a8ac2a1921f20f07d254399b
uventura <ualivent@gmail.com>
2025-10-02 16:37:23 -0300

    Adding files in array

--- auri/auri.c
+++ auri/auri.c
@@ -1,5 +1,10 @@
 #include "auri/cli.h"
 
+#include <stdio.h>
+
 int main(int argc, char* argv[]) {
-    auri_cli(argc, argv);
+    AuriCli cli = auri_cli(argc, argv);
+    for(uint32_t i = 0; i < cli.files_count; ++i) {
+        printf(">> %s\n", cli.file_paths[i]);
+    }
 }
